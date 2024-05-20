7041507a819172a8e5ab31c19d7ce6a9ffd1e5b0
agustinushenan01 <agustinushenan01@gmail.com>
2024-05-20 17:03:46 +0800

    update footer by Agung

--- my-project/src/components/Footer/Footer.jsx
+++ my-project/src/components/Footer/Footer.jsx
@@ -2,6 +2,8 @@ function Footer() {
     return (
         <main>
             <h1>Header</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, nostrum.</p>
+            <h3>By Agung</h3>
         </main>
     )
 }
