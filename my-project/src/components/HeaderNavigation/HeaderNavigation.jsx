352b6beccb3f6bcbf1d6cb467ee1ae60ef7a932a
neb <nabraskaamarilys@gmail.com>
2024-05-28 22:15:24 +0700

    tes update

--- my-project/src/components/HeaderNavigation/HeaderNavigation.jsx
+++ my-project/src/components/HeaderNavigation/HeaderNavigation.jsx
@@ -1,9 +1,27 @@
-
-import React from 'react'
-
 export default function HeaderNavigation() {
   return (
-    <div>HeaderNavigation oleh nabraska</div>
+    <div className="Navbar w-96 h-20 bg-white justify-center items-center gap-32 inline-flex">
+    <img className="LogoSampingHitam2 w-64 h-14" src="my-project/src/assets/logo.png" />
+    <div className="Frame1707478187 h-10 justify-center items-center gap-16 flex">
+      <div className="Frame1707478184 p-2.5 justify-center items-center gap-2.5 flex">
+        <div className="Home text-zinc-900 text-xl font-normal font-['Montserrat'] leading-tight">Home</div>
+      </div>
+      <div className="Frame1707478185 p-2.5 justify-center items-center gap-2.5 flex">
+        <div className="Benefit text-zinc-900 text-xl font-normal font-['Montserrat'] leading-tight">Benefit</div>
+      </div>
+      <div className="Frame1707478186 p-2.5 justify-center items-center gap-2.5 flex">
+        <div className="Features text-zinc-900 text-xl font-normal font-['Montserrat'] leading-tight">Features</div>
+      </div>
+      <div className="Frame1707478188 p-2.5 justify-center items-center flex">
+        <div className="Testimoni text-zinc-900 text-xl font-normal font-['Montserrat'] leading-tight">Testimoni</div>
+      </div>
+    </div>
+    <div className="Frame1707478252 justify-start items-start gap-3.5 flex">
+      <div className="Frame1707478251 p-2.5 bg-yellow-400 rounded-2xl justify-center items-center gap-2.5 flex">
+        <div className="GetTheApps text-zinc-900 text-xl font-normal font-['Montserrat'] leading-tight">Get The Apps</div>
+      </div>
+    </div>
+  </div>
   )
 }
 
