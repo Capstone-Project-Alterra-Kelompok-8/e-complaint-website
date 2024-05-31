2b3af586810867aca80e8e70e244a3a184c516ce
agustinushenan01 <agustinushenan01@gmail.com>
2024-05-31 13:55:51 +0800

    update super admin

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,14 +1,16 @@
 //! Setting router itu disini, jangan gunakan file terpisah lagi
 
-import HeaderNavigation from './components/HeaderNavigation/HeaderNavigation.jsx'
-import Hero from './components/hero/Hero.jsx'
+// import HeaderNavigation from './components/HeaderNavigation/HeaderNavigation.jsx'
+// import Hero from './components/hero/Hero.jsx'
+import ListAdmin from "./pages/SuperAmin/ListAdmin"
 
 function App() {
 
   return (
     <main>
-      <HeaderNavigation />
-      <Hero />
+      {/* <HeaderNavigation /> */}
+      {/* <Hero /> */}
+      <ListAdmin />
     </main>
   )
 }
