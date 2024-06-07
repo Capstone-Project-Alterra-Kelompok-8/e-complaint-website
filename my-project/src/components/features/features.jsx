e9372771872eac7cb20e7c07a849787252910582
kirinosuki <Septian070903@gmail.com>
2024-06-07 22:22:17 +0700

    update features

--- my-project/src/components/features/features.jsx
+++ my-project/src/components/features/features.jsx
@@ -63,9 +63,9 @@ export default function features() {
         </div>
       </div>
 
-      {/* Phone Mockups */}
+      {/* Phone Mockupss */}
       <div className="relative pl-20 ml-auto   ">
-        <img src={iphone} alt="image" className=" pb-20 " />
+        <img src={iphone} alt="image" className=" pb-20  " />
       </div>
     </div>
   );
