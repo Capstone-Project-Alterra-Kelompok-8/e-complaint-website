9a18cb6383e6c222a2209599f6802e73066ea5e5
neb <nabraskaamarilys@gmail.com>
2024-06-07 11:30:45 +0700

    update header
    memperbaiki logic title header

--- my-project/src/Layouts/headerDashboard.jsx
+++ my-project/src/Layouts/headerDashboard.jsx
@@ -1,7 +1,22 @@
+import { useLocation } from "react-router-dom"
+
 export default function Header(){
+    const location = useLocation();
+
+    const getTitle = (path) =>{
+        switch(path){
+            case '/dashboard' : return 'Dashboard';
+            case '/complaint' : return 'Complaint';
+            case '/chat-user' : return 'Chat Page';
+            case '/category' : return 'Category';
+            case '/news' : return 'News';
+
+            default :return 'Dashboard';
+        }
+    }
     return(
-        <div className=" bg-main-color p-4 flex justify-between items-center w-[850px]">
-            <div className="title text-zinc-900 text-3xl font-bold font-['Poppins']">Dashboard</div>
+        <div className=" bg-main-color p-4 flex justify-between items-center w-[1152px]">
+            <div className="title text-zinc-900 text-3xl font-bold font-['Poppins']">{getTitle(location.pathname)}</div>
             <img src="https://storage.googleapis.com/e-complaint-assets/profile-photos/admin-default.jpg" 
             alt="profile"
             className="w-12 h-12 rounded-full" />
--- my-project/src/pages/dashboard/Dashboard.jsx
+++ my-project/src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,16 @@
 import Sidebar from "../../Layouts/sidebar"
 import Header from "../../Layouts/headerDashboard"
 
-export default function DashboardPages(){
-    return(
-        <div className="flex flex-row justify-between">
-            <Sidebar/>
-            <Header/>
+export default function DashboardPages() {
+    return (
+        <div className="flex flex-row-reverse">
+            <Header />
+            <div className="flex flex-col flex-grow">
+                <Sidebar />
+                <div className="flex-grow p-4">
+                    {/* Konten utama dashboard */}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
