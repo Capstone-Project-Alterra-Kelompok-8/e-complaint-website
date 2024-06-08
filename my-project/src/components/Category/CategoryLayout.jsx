601d2587449860e69725eea1909d55ffa0108ab0
Agustinus Henan <agustinushenan01@gmail.com>
2024-06-08 20:25:21 +0800

    update sementara karena api dari BE bermasalah

--- my-project/src/components/Category/CategoryLayout.jsx
+++ my-project/src/components/Category/CategoryLayout.jsx
@@ -1,14 +1,20 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { PlusIcon, TrashIcon, PencilSquareIcon } from '@heroicons/react/24/outline';
 import Swal from 'sweetalert2';
 
 function CategoryLayout() {
     const [category, setCategory] = useState([]);
+    const [isAdding, setIsAdding] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [currentCategory, setCurrentCategory] = useState(null);
+    const [newCategory, setNewCategory] = useState({
+        Name: '',
+        Description: ''
+    });
 
     useEffect(() => {
         fetchCategory();
     }, []);
-
 
     const fetchCategory = async () => {
         try {
@@ -51,9 +57,16 @@ function CategoryLayout() {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
 
-            setCategory(prevcategory => prevcategory.filter(category => category.id !== categoryId))
+            setCategory(prevCategory => prevCategory.filter(category => category.id !== categoryId));
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Kategori berhasil dihapus",
+                showConfirmButton: false,
+                timer: 1500
+            });
         } catch (error) {
-            console.error('Error deleting category: ', error)
+            console.error('Error deleting category: ', error);
         }
     };
 
@@ -89,16 +102,102 @@ function CategoryLayout() {
         }
     };
 
+    const handleNewCategoryInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewCategory({ ...newCategory, [name]: value });
+    };
+
+    console.log(newCategory)
+    const handleNewCategorySubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const token = sessionStorage.getItem('token');
+            const response = await fetch('https://capstone-dev.mdrizki.my.id/api/v1/complaint-categories', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(newCategory)
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            console.log("tambah ", newCategory);
+            setNewCategory({
+                Name: '',
+                Description: ''
+            });
+            setIsAdding(false);
+            fetchCategory();
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Kategori berhasil ditambahkan",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        } catch (error) {
+            console.error('Error creating new category:', error);
+        }
+    };
+
+    const handleEditCategory = (category) => {
+        setCurrentCategory(category);
+        setIsEditing(true);
+    };
+
+    const handleEditCategoryInputChange = (e) => {
+        const { name, value } = e.target;
+        setCurrentCategory({ ...currentCategory, [name]: value });
+    };
+
+    const handleEditCategorySubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const token = sessionStorage.getItem('token');
+            const response = await fetch(`https://capstone-dev.mdrizki.my.id/api/v1/complaint-categories/${currentCategory.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(currentCategory)
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            setIsEditing(false);
+            setCurrentCategory(null);
+            fetchCategory();
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Kategori berhasil diperbarui",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        } catch (error) {
+            console.error('Error updating category:', error);
+        }
+    };
+
     return (
         <main>
             <section>
                 <button
                     type="button"
                     className="flex bg-main-color py-2 pl-4 pr-6 rounded-md mb-4 hover:bg-main-darker"
-                // onClick={() => setIsAdding(true)} // Menampilkan form tambah akun baru
+                    onClick={() => setIsAdding(true)}
                 >
                     <PlusIcon className="size-6 text-black" />
-                    <p className="font-medium text-black font-montserrat">Add Category</p>
+                    <p className="font-medium text-black font-montserrat">Tambah Kategori</p>
                 </button>
             </section>
             <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-8 gap-2 md:gap-3 lg:gap-4">
@@ -107,7 +206,7 @@ function CategoryLayout() {
                         <p className="text-main-color">{category.Name}</p>
                         <p>{category.Description}</p>
                         <div className="flex">
-                            <button>
+                            <button onClick={() => handleEditCategory(category)}>
                                 <PencilSquareIcon className="size-6 text-blue-500" />
                             </button>
                             <button onClick={() => deleteCategory(category.id)}>
@@ -117,9 +216,80 @@ function CategoryLayout() {
                     </div>
                 ))}
             </div>
-        </main>
 
-    )
+            {isAdding && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <form onSubmit={handleNewCategorySubmit} className="bg-white p-8 rounded shadow-md w-full max-w-md">
+                        <h2 className="text-2xl mb-4 font-bold">Tambah Kategori Baru</h2>
+                        <div className="mb-4">
+                            <label className="block mb-2 font-bold" htmlFor="Name">Nama</label>
+                            <input
+                                type="text"
+                                id="Name"
+                                name="Name"
+                                value={newCategory.Name}
+                                onChange={handleNewCategoryInputChange}
+                                className="w-full p-2 border border-gray-300 rounded"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block mb-2 font-bold" htmlFor="Description">Deskripsi</label>
+                            <input
+                                type="text"
+                                id="Description"
+                                name="Description"
+                                value={newCategory.Description}
+                                onChange={handleNewCategoryInputChange}
+                                className="w-full p-2 border border-gray-300 rounded"
+                                required
+                            />
+                        </div>
+                        <div className="flex justify-end">
+                            <button type="button" onClick={() => setIsAdding(false)} className="mr-4 px-4 py-2 text-gray-600">Batal</button>
+                            <button type="submit" className="px-4 py-2 bg-main-color text-white rounded">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            )}
+
+            {isEditing && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <form onSubmit={handleEditCategorySubmit} className="bg-white p-8 rounded shadow-md w-full max-w-md">
+                        <h2 className="text-2xl mb-4 font-bold">Edit Kategori</h2>
+                        <div className="mb-4">
+                            <label className="block mb-2 font-bold" htmlFor="Name">Nama</label>
+                            <input
+                                type="text"
+                                id="Name"
+                                name="Name"
+                                value={currentCategory.Name}
+                                onChange={handleEditCategoryInputChange}
+                                className="w-full p-2 border border-gray-300 rounded"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block mb-2 font-bold" htmlFor="Description">Deskripsi</label>
+                            <input
+                                type="text"
+                                id="Description"
+                                name="Description"
+                                value={currentCategory.Description}
+                                onChange={handleEditCategoryInputChange}
+                                className="w-full p-2 border border-gray-300 rounded"
+                                required
+                            />
+                        </div>
+                        <div className="flex justify-end">
+                            <button type="button" onClick={() => setIsEditing(false)} className="mr-4 px-4 py-2 text-gray-600">Batal</button>
+                            <button type="submit" className="px-4 py-2 bg-main-color text-white rounded">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            )}
+        </main>
+    );
 }
 
 export default CategoryLayout;
