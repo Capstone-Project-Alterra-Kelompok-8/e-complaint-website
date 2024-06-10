fdfd435ddb6f50eb9a36f127b2f10bc16f2bc655
daffaabp <daffa.budi2003@gmail.com>
2024-06-10 10:59:33 +0700

    feature

--- my-project/src/pages/HomePage.jsx
+++ my-project/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import HeaderNavigation from '../components/HeaderNavigation/HeaderNavigation'
 import Footer from '../components/Footer/Footer'
 import Hero from '../components/hero/Hero'
 import Benefit from '../components/Benefit/Benefit'
-import Features from '../components/Features/Features'
+
 
 const HomePage = () => {
   return (
@@ -10,7 +10,7 @@ const HomePage = () => {
     <HeaderNavigation />
     <Hero/>
     <Benefit/>
-    <Features/>
+    
     <Footer />
     </>
   )
