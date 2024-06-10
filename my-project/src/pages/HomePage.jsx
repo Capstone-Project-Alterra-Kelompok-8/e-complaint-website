24c47b49b863f37754d7a64d6824b8d34b791659
daffaabp <daffa.budi2003@gmail.com>
2024-06-10 10:40:29 +0700

    merge section benefit

--- my-project/src/pages/HomePage.jsx
+++ my-project/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import HeaderNavigation from '../components/HeaderNavigation/HeaderNavigation'
 import Footer from '../components/Footer/Footer'
 import Hero from '../components/hero/Hero'
+import Benefit from '../components/Benefit/Benefit'
 
 const HomePage = () => {
   return (
     <>
     <HeaderNavigation />
     <Hero/>
+    <Benefit />
     <Footer />
     </>
   )
