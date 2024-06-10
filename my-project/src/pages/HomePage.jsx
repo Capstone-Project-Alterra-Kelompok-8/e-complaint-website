e9211a613a0463b5ab8889ab9b9b02991598bd8b
daffaabp <daffa.budi2003@gmail.com>
2024-06-10 11:17:06 +0700

    merge section benefit, features, dan testimoni

--- my-project/src/pages/HomePage.jsx
+++ my-project/src/pages/HomePage.jsx
@@ -1,18 +1,16 @@
 import HeaderNavigation from '../components/HeaderNavigation/HeaderNavigation'
 import Footer from '../components/Footer/Footer'
 import Hero from '../components/hero/Hero'
-import Benefit from '../components/benefit/benefit'
+import Benefits from '../components/Benefits/Benefits'
 import Features from '../components/Feature/Feature'
 import Testimoni from '../components/Testimoni/Testimoni'
-
-
 
 const HomePage = () => {
   return (
     <>
     <HeaderNavigation />
     <Hero/>
-    <Benefit/>
+    <Benefits/>
     <Features/>
     <Testimoni/>
     <Footer />
