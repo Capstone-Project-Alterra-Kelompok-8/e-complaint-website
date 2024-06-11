88387d2bf49db27b299f9c6def0e0e3463fb6304
neb <nabraskaamarilys@gmail.com>
2024-06-11 15:54:38 +0700

    update slicing berita
    memperbaiki tampilan dan layout

--- my-project/src/components/Berita/cardNews.jsx
+++ my-project/src/components/Berita/cardNews.jsx
@@ -1,7 +1,7 @@
 export default function CardNews() {
     return (
-        <div className="card w-[486px] px-5 py-8 bg-slate-50 rounded-2xl gap-5 flex-col shadow inline-flex">
-            <img className="w-full h-48 relative rounded-lg shadow" src="https://via.placeholder.com/446x189" />
+        <div className="lg:w-[486px] px-5 py-8 bg-slate-50 rounded-2xl gap-5 flex-col shadow inline-flex">
+            <img className="w-full h-48 rounded-lg shadow" src="https://via.placeholder.com/446x189" />
             <div className="w-full flex-col justify-start items-start gap-4 inline-flex">
                 <div className="text-center text-black text-3xl font-medium font-poppins leading-tight tracking-tight">Parkir Liar</div>
                 <div className="text-justify text-black text-base font-medium font-montserrat leading-7">Parkir liar mengganggu ketertiban lalu lintas dan mengurangi kenyamanan serta keamanan bagi pengguna jalan lainnya.</div>
--- my-project/src/pages/dashboard/news.jsx
+++ my-project/src/pages/dashboard/news.jsx
@@ -1,5 +1,8 @@
 import CardNews from "../../components/Berita/cardNews";
 import ButtonNews from "../../components/Berita/buttonNews";
+import Sidebar from "../../Layouts/sidebar"
+import Header from "../../Layouts/headerDashboard";
+import { IoSearch } from "react-icons/io5";
 const beritaData = [
     {
         id: 1,
@@ -40,34 +43,39 @@ const beritaData = [
     // Tambahkan lebih banyak data sesuai kebutuhan
 ];
 
-const App = () => {
+const NewsPage = () => {
     return (
         <>
-
-        <div className="bg-light-2 ps-16 pt-12">
-            <div className="font-poppins text-black text-4xl font-medium">Kelola Berita</div>
-            <div className="container mt-12 bg-gray-300 justify-between px-5 pt-5 rounded-lg w-full">
-                <div className="container flex justify-between items-center mb-5">
-                    <ButtonNews />
-                    <input type="text" className="w-80 p-2 border border-gray-300 rounded" placeholder="Kata kunci atau tracking ID" />
-                </div>
-
-                <div className="container w-full flex flex-wrap gap-5">
-                    {beritaData.map((berita) => (
-                        <div key={berita.id} className="p-4">
-                            <CardNews
-                                image={berita.image}
-                                title={berita.title}
-                                description={berita.description}
-                            />
+            <Sidebar></Sidebar>
+            <Header></Header>
+            <div className="bg-light-2">
+                <div className="lg:ml-80 pt-9 font-poppins text-black text-4xl font-medium">Kelola Berita</div>
+                <div className="container mt-9 lg:ml-80 w-fit bg-gray-300 justify-between px-5 pt-5 rounded-lg">
+                    <div className="container lg:flex justify-between items-center mb-5">
+                        <ButtonNews />
+                        <div className="flex mt-5 lg:mt-0 px-2 lg:w-80 items-center rounded border border-gray-400 bg-white">
+                            <span className="text-2xl"><IoSearch />
+                            </span>
+                            <input type="text" className=" lg:w-80 border-none" placeholder="Kata kunci atau tracking ID" />
                         </div>
-                    ))}
+                    </div>
+
+                    <div className=" container w-full flex flex-wrap gap-2">
+                        {beritaData.map((berita) => (
+                            <div key={berita.id} className="p-4">
+                                <CardNews
+                                    image={berita.image}
+                                    title={berita.title}
+                                    description={berita.description}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
         </>
-        
+
     );
 }
 
-export default App;
+export default NewsPage;
