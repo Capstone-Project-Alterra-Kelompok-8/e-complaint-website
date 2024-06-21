b1eecb6ba854af6ab8706ea17c421b127e6b1855
daffaabp <daffa.budi2003@gmail.com>
2024-06-21 11:50:02 +0700

    re design ulang page dashboard dan card progress

--- my-project/src/pages/Dashboard.jsx
+++ my-project/src/pages/Dashboard.jsx
@@ -1,11 +1,22 @@
-import Progress from '../components/Dashboard/Progress'
+import React from 'react';
+import Progress from '../components/Dashboard/Progress';
+import HeaderLayout from '../components/Header/HeaderLayout';
+import SidebarLayout from '../components/Header/SidebarLayout';
 
 const Dashboard = () => {
   return (
     <>
-      <Progress />
+      <section className="flex w-full min-h-screen">
+        <SidebarLayout />
+        <div className="flex flex-col flex-grow lg:ml-[287px]">
+          <HeaderLayout />
+          <div className="flex-grow bg-light-2 p-8 overflow-y-auto">
+            <Progress />
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
