9a8fcb5e58e6b23719cb450cc40c38f755d5d068
Agustinus Henan <agustinushenan01@gmail.com>
2024-06-21 12:28:32 +0800

    fix error

--- my-project/src/components/Benefits/Benefits.jsx
+++ my-project/src/components/Benefits/Benefits.jsx
@@ -3,7 +3,6 @@ import logo2 from '../../assets/logo3.png';
 import logo3 from '../../assets/logo4.png';
 import logo4 from '../../assets/logo5.png';
 import iphoneImage from '../../assets/iphone-image2.png';
-import iphoneImage2 from '../../assets/iphone-image3.png';
 
 const Benefits = () => {
   return (
@@ -18,28 +17,28 @@ const Benefits = () => {
         <div className="flex flex-col">
           <div className="text-justify text-zinc-900 text-2xl font-bold font-montserrat leading-normal pl-5 pt-24">Mengapa harus memilih Aplikasi ini?</div>
 
-          <div class="flex flext-row justify-between pt-24">
-            <div class="flex-auto w-80 ...">
+          <div className="flex flext-row justify-between pt-24">
+            <div className="flex-auto w-80 ...">
               <img src={logo} alt="image" className="w-30 h-16 pl-16 flex flex-row" />
               <div className="text-justify text-zinc-800 text-xl font-semibold font-montserrat leading-normal "> Kemudahan Akses </div>
               <p className="font-montserrat text-left text-sm">Kemudahan akses memungkinkan pengguna mengajukan aduan kapan saja dan dari mana saja</p>
             </div>
 
-            <div class="flex-auto w-80 ...">
+            <div className="flex-auto w-80 ...">
               <img src={logo2} alt="image" className="w-30 h-16 pl-16 flex flex-row" />
               <div className="text-justify text-zinc-800 text-xl font-semibold font-montserrat leading-normal "> Penanganan yang Cepat </div>
               <p className="font-montserrat text-left text-sm">Penanganan cepat meningkatkan respons, efisiensi, dan kepuasan pengguna.</p>
             </div>
           </div>
 
-          <div class="flex flext-row justify-between pt-24">
-            <div class="flex-auto w-80 ...">
+          <div className="flex flext-row justify-between pt-24">
+            <div className="flex-auto w-80 ...">
               <img src={logo3} alt="image" className="w-30 h-16 pl-16 flex flex-row" />
               <div className="text-justify text-zinc-800 text-xl font-semibold font-montserrat leading-normal "> Keamanan Data </div>
               <p className="font-montserrat text-left text-sm">Keamanan data melindungi informasi sensitif dari akses, penggunaan, menjaga privasi dan integritas data.</p>
             </div>
 
-            <div class="flex-auto w-80 ...">
+            <div className="flex-auto w-80 ...">
               <img src={logo4} alt="image" className="w-30 h-16 pl-16 flex flex-row" />
               <div className="text-justify text-zinc-800 text-xl font-semibold font-montserrat leading-normal "> Feedback dan Evaluas </div>
               <p className="font-montserrat text-left text-sm">Feedback dan evaluasi membantu meningkatkan kualitas layanan dan produk dengan mengidentifikasi area yang perlu diperbaiki.</p>
