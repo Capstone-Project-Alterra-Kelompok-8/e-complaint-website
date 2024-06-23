5856e45d465227e5357b77734fd54470f987b626
kirinosuki <Septian070903@gmail.com>
2024-06-24 06:22:25 +0700

    update responsif

--- my-project/src/components/Benefits/Benefits.jsx
+++ my-project/src/components/Benefits/Benefits.jsx
@@ -2,18 +2,20 @@ import logo from '../../assets/logo2.png';
 import logo2 from '../../assets/logo3.png';
 import logo3 from '../../assets/logo4.png';
 import logo4 from '../../assets/logo5.png';
-import iphoneImage from '../../assets/iphone-image2.png';
+import iphoneImage from '../../assets/gambar-titik.png';
 
 const Benefits = () => {
   return (
-    <div className="h-[670px] bg-white flex flex-row justify-between ps-36 pe-20 relative" id="benefit">
+    <div className="h-[670px] bg-white flex flex-row justify-between ps-16 pe-20 relative" id="benefit">
       {/* Phone Mockups */}
-      <div className="relative mb-0 mt-auto ps-20">
-        <img src={iphoneImage} alt="image" />
+      <div className="relative mb-0 mt-auto ">
+        <div className="relative">
+          <img src={iphoneImage} alt="image" className="w-[600px] h-[670px] max-w-full pb-16 pt-14 mr-10 " />
+        </div>
       </div>
 
       {/* Content */}
-      <div className="relative flex flex-row justify-start  w-[578px] gap-24 pr-20">
+      <div className="relative flex flex-row justify-start  w-[578px] gap-24 pr-20 ">
         <div className="flex flex-col">
           <div className="text-justify text-zinc-900 text-2xl font-bold font-montserrat leading-normal pl-5 pt-24">Mengapa harus memilih Aplikasi ini?</div>
 
@@ -39,15 +41,15 @@ const Benefits = () => {
             </div>
 
             <div className="flex-auto w-80 ...">
-              <img src={logo4} alt="image" className="w-30 h-16 pl-16 flex flex-row" />
-              <div className="text-justify text-zinc-800 text-xl font-semibold font-montserrat leading-normal "> Feedback dan Evaluas </div>
+              <img src={logo4} alt="image" className="w-30 h-16 pl-16  flex flex-row" />
+              <div className="text-justify text-zinc-800 text-xl font-semibold font-montserrat leading-normal "> Feedback dan Evaluasi </div>
               <p className="font-montserrat text-left text-sm">Feedback dan evaluasi membantu meningkatkan kualitas layanan dan produk dengan mengidentifikasi area yang perlu diperbaiki.</p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Benefits
+export default Benefits;
