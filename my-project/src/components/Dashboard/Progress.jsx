b9b6661366cf4f78c7b86ba554299c63259a910e
daffaabp <daffa.budi2003@gmail.com>
2024-07-02 13:56:39 +0700

    perbaiki card progress pada dashboard

--- my-project/src/components/Dashboard/Progress.jsx
+++ my-project/src/components/Dashboard/Progress.jsx
@@ -2,31 +2,35 @@ import CardProgress from "./CardProgress";
 
 const Progress = ({ progressData }) => {
   return (
-    <div className="justify-center w-full flex-wrap flex flex-col lg:flex-row gap-4">
-        <CardProgress
-          number={progressData.Pending}
-          text="Laporan Diterima"
-          icon="/images/laporan-diterima.svg"
-          backgroundImage="/images/awan-akatsuki.png"
-        />
-        <CardProgress
-          number={progressData.Verifikasi}
-          text="Terverifikasi"
-          icon="/images/terverifikasi.svg"
-          backgroundImage="/images/awan-akatsuki.png"
-        />
-        <CardProgress
-          number={progressData["On Progress"]}
-          text="Sedang Diproses"
-          icon="/images/detail-laporan.svg"
-          backgroundImage="/images/awan-akatsuki.png"
-        />
-        <CardProgress
-          number={progressData.Selesai}
-          text="Laporan Selesai"
-          icon="/images/laporan-selesai.svg"
-          backgroundImage="/images/awan-akatsuki.png"
-        />
+    <div className="flex flex-col lg:flex-row flex-wrap justify-center w-full gap-4">
+      {progressData && (
+        <>
+          <CardProgress
+            number={progressData.Pending || 0}
+            text="Laporan Diterima"
+            icon="/images/laporan-diterima.svg"
+            backgroundImage="/images/awan-akatsuki.png"
+          />
+          <CardProgress
+            number={progressData.Verifikasi || 0}
+            text="Terverifikasi"
+            icon="/images/terverifikasi.svg"
+            backgroundImage="/images/awan-akatsuki.png"
+          />
+          <CardProgress
+            number={progressData["On Progress"] || 0}
+            text="Sedang Diproses"
+            icon="/images/detail-laporan.svg"
+            backgroundImage="/images/awan-akatsuki.png"
+          />
+          <CardProgress
+            number={progressData.Selesai || 0}
+            text="Laporan Selesai"
+            icon="/images/laporan-selesai.svg"
+            backgroundImage="/images/awan-akatsuki.png"
+          />
+        </>
+      )}
     </div>
   );
 };
